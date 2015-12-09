c9f6c22eff8a65a65a0d4e0ab11256b257feb6cb
TED <tedlade.tl@gmail.com>
2015-12-09 14:04:15 +0100

    Redesigned options

--- dev/modules/dashboard/dashboard.js
+++ dev/modules/dashboard/dashboard.js
@@ -12,8 +12,11 @@ angular.module('app.dashboard', ['ui.router'])
 
 .controller('DashboardCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
     $scope.labels = ["11/9", "11/10", "11/11", "11/12", "11/13", "11/14", "Today"];
-    $scope.data = [
-        [70, 100, 30, 50, 60, 90, 10]
-    ];
+    $scope.data = {
+        "today" : [70, 100, 30, 50, 60, 90, 10],
+        "yesterday" : [70, 100, 40, 90, 60, 90, 10],
+        "lastweek" : [70, 50, 30, 20, 60, 90, 10],
+        "lastmonth" : [70, 10, 30, 90, 60, 90, 10]
+    };
     $scope.series = ['Transaction Amount'];
 }])
