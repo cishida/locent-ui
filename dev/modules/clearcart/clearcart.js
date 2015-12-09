5ff7d9d1177a97f5a36e8b86b9b57cbae7a14011
TED <tedlade.tl@gmail.com>
2015-12-09 01:28:06 +0100

    Routing

--- dev/modules/clearcart/clearcart.js
+++ dev/modules/clearcart/clearcart.js
@@ -5,11 +5,33 @@ angular.module('app.clearcart', ['ui.router'])
         $stateProvider.state('app.clearcart', {
             url: '/clearcart-checkout',
             templateUrl: 'modules/clearcart/index.html',
-            controller: 'clearcartCtrl'
+            controller: 'ClearcartCtrl',
+            redirectTo: 'app.clearcart.analytics'
+        }).state('app.clearcart.analytics', {
+            url: '/analytics',
+            templateUrl: 'modules/clearcart/analytics.html',
+            controller: 'ClearcartAnalyticsCtrl'
+        }).state('app.clearcart.messages', {
+            url: '/messages',
+            templateUrl: 'modules/clearcart/messages.html',
+            controller: 'ClearcartMessagesCtrl'
+        }).state('app.clearcart.options', {
+            url: '/options',
+            templateUrl: 'modules/clearcart/options.html',
+            controller: 'ClearcartOptionsCtrl'
         })
     }
 ])
 
-.controller('clearcartCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
-    console.log("clearcart View")
+.controller('ClearcartCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
+    
 }])
+.controller('ClearcartAnalyticsCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
+    $scope.labels = ["11/9", "11/10", "11/11", "11/12", "11/13", "11/14", "Today"];
+    $scope.data = [
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
+}])
+.controller('ClearcartMessagesCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {}])
+
+.controller('ClearcartOptionsCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParam) {}])
--- dev/modules/safetext/safetext.js
+++ dev/modules/safetext/safetext.js
@@ -5,11 +5,33 @@ angular.module('app.safetext', ['ui.router'])
         $stateProvider.state('app.safetext', {
             url: '/safetext-checkout',
             templateUrl: 'modules/safetext/index.html',
-            controller: 'safetextCtrl'
+            controller: 'SafetextCtrl',
+            redirectTo: 'app.safetext.analytics'
+        }).state('app.safetext.analytics', {
+            url: '/analytics',
+            templateUrl: 'modules/safetext/analytics.html',
+            controller: 'SafetextAnalyticsCtrl'
+        }).state('app.safetext.messages', {
+            url: '/messages',
+            templateUrl: 'modules/safetext/messages.html',
+            controller: 'SafetextMessagesCtrl'
+        }).state('app.safetext.options', {
+            url: '/options',
+            templateUrl: 'modules/safetext/options.html',
+            controller: 'SafetextOptionsCtrl'
         })
     }
 ])
 
-.controller('safetextCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
+.controller('SafetextCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
     console.log("safetext View")
 }])
+.controller('SafetextAnalyticsCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
+    $scope.labels = ["11/9", "11/10", "11/11", "11/12", "11/13", "11/14", "Today"];
+    $scope.data = [
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
+}])
+.controller('SafetextMessagesCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {}])
+
+.controller('SafetextOptionsCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParam) {}])
