f26ad3799ecdf2401ca21958dc1814acdfedf704
TED <tedlade.tl@gmail.com>
2015-12-15 00:07:05 +0100

    created route for create-campaign

--- dev/modules/campaigns/campaigns.js
+++ dev/modules/campaigns/campaigns.js
@@ -6,10 +6,18 @@ angular.module('app.campaigns', ['ui.router'])
             url: '/campaigns',
             templateUrl: 'modules/campaigns/index.html',
             controller: 'CampaignsCtrl'
+        }).state('app.campaigns.create-campaign', {
+            url: '/create-campaign',
+            templateUrl: 'modules/campaigns/create-campaign.html',
+            controller: 'CampaignCreateCtrl'
         })
     }
 ])
 
 .controller('CampaignsCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
+    
+}])
+
+.controller('CampaignCreateCtrl', ['$state', '$scope', '$stateParams', function($state, $scope, $stateParams) {
     
 }])
